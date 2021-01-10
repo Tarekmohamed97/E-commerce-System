fc9d14d1a95f02d5aa3ef2b48f7e1fac360c4d51
Tarekmohamed97 <tm7271420@gmail.com>
2021-01-11 00:13:37 +0200

    implementing the functions for formating the path for each product

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,10 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import data from "../../products.json";
+import tagsData from "../../tagstree.json";
 
 export default class Products extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products: data.products,
+            tags: tagsData
+        }
+    }    
+    
+    //function to reform the tags to one layer array and each element is a full path for the tag and the children
+    extractingPaths = (node, nodeFullPath) => {
+        nodeFullPath += node.alias + "/";
+        if(!node.children){
+            return nodeFullPath
+        }
+        node.children.forEach(child => {
+            if(!child.children){
+                nodeFullPath = nodeFullPath  + child.alias + "/"
+            } else{
+                nodeFullPath = "/" + this.extractingPaths(child, nodeFullPath)
+            }
+        });
+        return nodeFullPath
+    }
+
+    formingProductPath = (product, pathArray) => {
+        let finalPath = "";
+
+        Array.prototype.forEach.call(product.tags, tag => {
+            Array.prototype.forEach.call(pathArray, path => {
+                console.log(tag.alias)
+                if(path.includes(tag.alias)){
+                    finalPath += path;
+                }
+            })
+            finalPath += tag.alias + "/"
+        });        
+        return finalPath;
+    }
+
     render() {
+        let pathArray = []
+        let nodeFullPath = "";
+        let finalProductPath = ""
+
+        
+        Array.prototype.forEach.call(this.state.tags, tag => {
+            pathArray.push(this.extractingPaths(tag, nodeFullPath));
+        })
+
+        Array.prototype.forEach.call(this.state.products, product => {
+            //finalProductPath = this.formingProductPath(product, nodeFullPath);
+        })
+        finalProductPath = this.formingProductPath(this.state.products[0], pathArray);
+        console.log(finalProductPath);
+                
         return (
             <div>
-                products
+               products
             </div>
         )
     }
