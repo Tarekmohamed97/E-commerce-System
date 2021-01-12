41d1691a3a112110b024976fda5422898a942b97
Tarekmohamed97 <tm7271420@gmail.com>
2021-01-12 22:34:44 +0200

    adding the nec customizedLoader component to the lazy loading

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,21 +1,39 @@
-import React, {lazy, Suspense} from 'react';
+import React, {lazy, Suspense, Component} from 'react';
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Footer from '../components/layout/footer';
 import Header from '../components/layout/header';
+import CustomizedLoader from '../components/customizedLoader'
 const Products = lazy(() => import("../components/products"));
 
-function Pages() {
-    return (
-        <BrowserRouter>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Header/>
-                <Switch>
-                    <Route exact path="/products" component = {Products}/>
-                </Switch>
-                <Footer/>
-            </Suspense>
-        </BrowserRouter>
-    )
+
+export default class pages extends Component{
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentURL: ""
+        }
+    }    
+
+    componentDidMount() {
+        this.setState({currentURL: window.location.pathname});   
+    }
+
+
+
+    render(){
+        return (
+            <BrowserRouter>
+                <Suspense fallback={<div><CustomizedLoader size="large" /></div>}>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component = {Products}/>
+                    </Switch>
+                    <Footer/>
+                </Suspense>
+            </BrowserRouter>
+        )
+    }
 }
 
-export default Pages
